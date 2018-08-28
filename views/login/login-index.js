542cb4aa8c7446e4e3ba482a88c425a41100037d
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2018-08-28 08:25:42 -0300

    Reajustes nas telas iniciais e template de email

--- views/login/login-index.js
+++ views/login/login-index.js
@@ -7,11 +7,11 @@
     }
 });
 
-
+*/
 $(window).on('load',function(){
     $('#ModalCadastro').modal('show');
 });
-*/
+
 
 $('.tags-editor-textarea').tagEditor({
     placeholder: 'Digite aqui'
@@ -33,57 +33,9 @@ $('#motivos-textarea').simplyCountable({
 });
 
 $(document).ready(function () {
-// Mascaramento dos campos
-    $('#cpf').mask('000.000.000-00', {reverse: true, placeholder: "000.000.000-00"});
-    $('.telefone').mask('(00) 00000-0000', {reverse: false, placeholder: "(00) 00000-0000"});
 
 
-// Validador de campos
-
-
-    $("#cadastro-form").validate({
-        rules: {
-            selectvinculo: {
-                selectcheck: true
-            },
-
-            cpf: "required"
-        },
-        messages: {
-            cpf: "O CPF é importante para o acesso"
-        },
-        errorElement: "em",
-        errorPlacement: function (error, element) {
-            // Add the `help-block` class to the error element
-            error.addClass("help-block");
-
-            if (element.prop("type") === "checkbox") {
-                error.insertAfter(element.parent("label"));
-            } else {
-                error.insertAfter(element);
-            }
-        },
-        errorPlacement: function (error, element) {
-            if (element.hasClass('select2')) {
-                error.insertAfter(element.next('span'));  // select2
-            } else {
-                error.insertAfter(element);               // default
-            }
-        },
-        highlight: function (element, errorClass, validClass) {
-            $(element).parents(".form-group").addClass("has-danger").removeClass("has-success");
-        },
-        unhighlight: function (element, errorClass, validClass) {
-            $(element).parents(".form-group").addClass("has-success").removeClass("has-danger");
-        }
-    });
-
-// Adiciona o select2 na validação
-    jQuery.validator.addMethod('selectcheck', function (value) {
-        return (value != '0');
-    }, "Selecione ao menos um vínculo");
-
-// alertas quando deletar
+// alertas quando enviar
     $('.swal-btn-cancel').click(function (e) {
         e.preventDefault();
         swal({
